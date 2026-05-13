a9c4698e66cde60f2f07f96c652ece1462f4bf97
Kotte Pravallika <146621306+prav-kotte1@users.noreply.github.com>
2026-05-13 13:12:37 +0530

    Time: 0 ms (100.00%) | Memory: 87.3 MB (10.26%) - LeetSync

---
+++ 890-lemonade-change/lemonade-change.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool lemonadeChange(vector<int>& bills) {
+        int f=0, t=0;
+        for(int x:bills){
+            if(x==5) f++;
+            else if (x==10){
+                if(f==0) return false;
+                f--; t++;
+            }
+            else {
+                if(t && f){
+                    t--; f--;
+                } else if(f >= 3) f-=3;
+                else return false;
+            }
+        }
+        return true;
+    }
+};
