0e74659989d6d2f549c7c879ee1dc66d6c60964e
Kotte Pravallika <146621306+prav-kotte1@users.noreply.github.com>
2026-05-16 15:25:21 +0530

    Time: 0 ms (100.00%) | Memory: 20.5 MB (31.42%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.cpp
+++ 45-jump-game-ii/jump-game-ii.cpp
@@ -2,15 +2,16 @@ class Solution {
 public:
     int jump(vector<int>& nums) {
         int n=nums.size();
-        vector<int> dp(n, INT_MAX);
-        dp[0]=0;
-
-        for(int i=0; i<n;i++){
-            for(int j=1; j<= nums[i] && i+j<n; ++j){
-                dp[i+j] = min(dp[i+j], dp[i]+1);
+        int ans=0, farthest=0, currEnd=0;
+        for(int i=0; i<n-1;i++){
+            farthest = max(farthest, i+nums[i]);
+            if(farthest >= n-1){
+                ++ans; break;
+            }
+            if(i==currEnd){
+                ++ans; currEnd = farthest;
             }
         }
-
-        return dp[n-1];
+        return ans;
     }
 };
