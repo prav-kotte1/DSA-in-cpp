1fc1444b6b26143a5e7dc005afe228b4684bd547
Kotte Pravallika <146621306+prav-kotte1@users.noreply.github.com>
2026-05-31 16:21:49 +0530

    Time: 4 ms (25.46%) | Memory: 19.8 MB (48.22%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.cpp
+++ 547-number-of-provinces/number-of-provinces.cpp
@@ -1,23 +1,55 @@
+// version 1: dfs soln
+// class Solution {
+// private:
+//     void dfs(int node, vector<vector<int>>& isC, vector<int>& vis){
+//         vis[node] = 1;
+//         for(int nei = 0; nei <isC.size(); nei++){
+//             if(isC[node][nei] == 1 && !vis[nei]){
+//                 dfs(nei, isC, vis);
+//             }
+//         }
+//     }
+// public:
+//     int findCircleNum(vector<vector<int>>& isC) {
+//         int n = isC.size();
+//         vector<int> vis(n, 0);
+//         int res = 0; 
+
+//         for(int i=0; i<n; i++){
+//             if(!vis[i]){
+//                 res++;
+//                 dfs(i, isC, vis);
+//             }
+//         }
+//         return res;
+//     }
+// };
+
+//version 2: bfs solution
 class Solution {
-private:
-    void dfs(int node, vector<vector<int>>& isC, vector<int>& vis){
-        vis[node] = 1;
-        for(int nei = 0; nei <isC.size(); nei++){
-            if(isC[node][nei] == 1 && !vis[nei]){
-                dfs(nei, isC, vis);
-            }
-        }
-    }
-public:
-    int findCircleNum(vector<vector<int>>& isC) {
+public: 
+    int findCircleNum(vector<vector<int>>& isC){
         int n = isC.size();
         vector<int> vis(n, 0);
-        int res = 0; 
+        int res = 0;
 
-        for(int i=0; i<n; i++){
+        for(int i =0; i<n;i++){
             if(!vis[i]){
                 res++;
-                dfs(i, isC, vis);
+                queue<int> q;
+                q.push(i);
+                vis[i]=1;
+
+                while(!q.empty()){
+                    int node = q.front();
+                    q.pop();
+                    for(int nei = 0; nei < n; nei++){
+                        if(isC[node][nei]==1 && !vis[nei]){
+                            vis[nei]=1;
+                            q.push(nei);
+                        }
+                    }
+                }
             }
         }
         return res;
