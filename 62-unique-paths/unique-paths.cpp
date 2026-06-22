e3b9882c7e8564b4aa8dda0d94011fb01b8c8fd1
Kotte Pravallika <146621306+prav-kotte1@users.noreply.github.com>
2026-06-22 15:10:10 +0530

    Time: 0 ms (100.00%) | Memory: 9.3 MB (52.62%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -1,15 +1,24 @@
 class Solution {
-    int fnc(int i, int j, vector<vector<int>>& dp){
-        if(i==0 && j==0) return 1; //base case
-        if(i<0 || j<0) return 0; //out of bounds case
-        if(dp[i][j] != -1) return dp[i][j]; //already computed case
-        int down = fnc(i-1, j, dp);
-        int right = fnc(i, j-1, dp);
-        return dp[i][j] = down + right;
+    int fnc(int m, int n, vector<vector<int>>& dp){
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(i==0 && j==0){
+                    dp[i][j] = 1; //base case
+                    continue;
+                } 
+                int up = 0, left = 0;
+                if(i > 0)
+                    up = dp[i-1][j];
+                if(j > 0)   
+                    left = dp[i][j-1];
+                dp[i][j] = up + left;
+            }
+        }
+        return dp[m-1][n-1];
     }
 public:
     int uniquePaths(int m, int n) {
         vector<vector<int>> dp(m, vector<int> (n, -1));
-        return fnc(m-1, n-1, dp);
+        return fnc(m, n, dp);
     }
 };
