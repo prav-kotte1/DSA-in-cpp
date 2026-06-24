a7f963a548105f4e84f2bf8c6ad8b625cdd15b72
Kotte Pravallika <146621306+prav-kotte1@users.noreply.github.com>
2026-06-24 12:40:12 +0530

    Time: 5 ms (71.27%) | Memory: 15.8 MB (37.15%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -1,27 +1,39 @@
 class Solution {
-private:
-    int f(int ind, vector<int>& nums, int target, vector<vector<int>>& dp){
-        if(ind == 0){
-            if(target == 0 && nums[0] == 0) return 2;
-            if(target == 0 || nums[0] == target) return 1;
-            return 0;
-        }
-        if(dp[ind][target] != -1) return dp[ind][target];
-        int notTake = f(ind-1, nums, target, dp);
-        int take = 0;
-        if(nums[ind] <= target){
-            take = f(ind-1, nums, target-nums[ind], dp);
-        }
-        return dp[ind][target] = take + notTake;
-    }
 public:
     int findTargetSumWays(vector<int>& nums, int target) {
-        int sum = 0;
-        for(int num : nums) sum += num;
-        int p = (sum + target)/2;
+
+        int sum = accumulate(nums.begin(), nums.end(), 0);
+
         if(sum + target < 0) return 0;
-        if((sum + target)%2) return 0;
-        vector<vector<int>> dp(nums.size(), vector<int> (p+1, -1));
-        return f(nums.size()-1, nums, p, dp);
+        if((sum + target) % 2) return 0;
+
+        int k = (sum + target) / 2;
+        int n = nums.size();
+
+        vector<vector<int>> dp(n, vector<int>(k + 1, 0));
+
+        // Base case
+        if(nums[0] == 0)
+            dp[0][0] = 2;
+        else
+            dp[0][0] = 1;
+
+        if(nums[0] != 0 && nums[0] <= k)
+            dp[0][nums[0]] = 1;
+
+        for(int i = 1; i < n; i++) {
+            for(int target = 0; target <= k; target++) {
+
+                int notTake = dp[i-1][target];
+
+                int take = 0;
+                if(nums[i] <= target)
+                    take = dp[i-1][target - nums[i]];
+
+                dp[i][target] = take + notTake;
+            }
+        }
+
+        return dp[n-1][k];
     }
 };
