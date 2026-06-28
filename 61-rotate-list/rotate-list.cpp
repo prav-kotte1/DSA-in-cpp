7951a19f99bfdc67bf6f4f0216419bb045d88c43
Kotte Pravallika <146621306+prav-kotte1@users.noreply.github.com>
2026-06-28 16:22:59 +0530

    Time: 0 ms (100.00%) | Memory: 16.4 MB (31.39%) - LeetSync

--- 61-rotate-list/rotate-list.cpp
+++ 61-rotate-list/rotate-list.cpp
@@ -9,23 +9,22 @@
  * };
  */
 class Solution {
- public:
-  ListNode* rotateRight(ListNode* head, int k) {
-    if (!head || !head->next || k == 0)
-      return head;
+public:
+    ListNode* rotateRight(ListNode* head, int k) {
+        if(!head || !head-> next|| k==0) return head;
 
-    ListNode* tail;
-    int length = 1;
-    for (tail = head; tail->next; tail = tail->next)
-      ++length;
-    tail->next = head;  // Circle the list.
-
-    const int t = length - k % length;
-    for (int i = 0; i < t; ++i)
-      tail = tail->next;
-    ListNode* newHead = tail->next;
-    tail->next = nullptr;
-
-    return newHead;
-  }
+        int n=1;
+        ListNode* tail =head;
+        while(tail->next){
+            tail=tail->next; n++;
+        }
+        k%=n;
+        if(k==0) return head;
+        tail->next=head;
+        ListNode* newTail=head;
+        for(int i=0; i<n-k-1;i++) newTail =newTail->next;
+        ListNode* newHead = newTail->next;
+        newTail->next = nullptr;
+        return newHead;
+    }
 };
