046b9d305d4b11adfa79ce5c41e6d856d31e7055
Kotte Pravallika <146621306+prav-kotte1@users.noreply.github.com>
2026-07-03 11:17:37 +0530

    Time: 123 ms (56.39%) | Memory: 13.9 MB (29.95%) - LeetSync

--- 312-burst-balloons/burst-balloons.cpp
+++ 312-burst-balloons/burst-balloons.cpp
@@ -1,20 +1,23 @@
 class Solution {
 public:
-    int f(int i, int j, vector<int>& nums, vector<vector<int>>& dp){
-        if(i > j) return 0;
-        if(dp[i][j] != -1) return dp[i][j];
-        int maxi = INT_MIN;
-        for(int k=i; k<=j; k++){
-            int coins = nums[i-1]*nums[k]*nums[j+1]+f(i, k-1, nums, dp) + f(k+1, j, nums, dp);
-            maxi = max(coins, maxi);
-        }
-        return dp[i][j] =  maxi;
-    }
     int maxCoins(vector<int>& nums) {
         nums.insert(nums.begin(), 1);
         nums.push_back(1);
         int n = nums.size();
-        vector<vector<int>> dp(n+2, vector<int>(n+2, -1));
-        return f(1, n-2, nums, dp);
+        vector<vector<int>> dp(n + 1, vector<int>(n + 1, -1));
+        for (int i = n - 2; i >= 1; i--) {
+            for (int j = i; j <= n - 2; j++) {
+                int maxi = 0;
+                for (int k = i; k <= j; k++) {
+                    int left = (k > i) ? dp[i][k - 1] : 0;
+                    int right = (k < j) ? dp[k + 1][j] : 0;
+                    int coins =
+                        nums[i - 1] * nums[k] * nums[j + 1] + left + right;
+                    maxi = max(coins, maxi);
+                }
+                dp[i][j] = maxi;
+            }
+        }
+        return dp[1][n - 2];
     }
 };
