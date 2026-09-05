cb07c98285a7a1f3ede03a7af65d3f16e824899d
Kotte Pravallika <146621306+prav-kotte1@users.noreply.github.com>
2026-09-05 12:26:54 +0530

    Time: 4 ms (13.51%) | Memory: 16.8 MB (69.11%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.cpp
+++ 1036-rotting-oranges/rotting-oranges.cpp
@@ -4,19 +4,18 @@ public:
         int n = grid.size();
         int m = grid[0].size();
 
-        queue<pair<pair<int, int>, int>> q;
+        queue<pair<pair<int,int>,int>> q;
         int fresh = 0;
 
         for(int i=0; i<n; i++){
             for(int j=0; j<m; j++){
-                if(grid[i][j] == 2){
+                if(grid[i][j] == 2)
                     q.push({{i, j}, 0});
-                } else if(grid[i][j] == 1){
+                else if(grid[i][j] == 1)
                     fresh++;
-                }
             }
         }
-
+        
         int time = 0;
         int dr[] = {-1, 0, 1, 0};
         int dc[] = {0, 1, 0, -1};
@@ -28,21 +27,17 @@ public:
             int row = front.first.first;
             int col = front.first.second;
             int t = front.second;
-
             time = max(time, t);
 
-            for(int k = 0; k<4 ; k++){
+            for(int k=0; k<4; k++){
                 int nr = row + dr[k];
                 int nc = col + dc[k];
 
-                if(nr >= 0 && nr < n &&
-                    nc >= 0 && nc < m &&
-                    grid[nr][nc] == 1){
-                        grid[nr][nc]=2;
-                        fresh--;
-
-                        q.push({{nr, nc}, t+1});
-                    }
+                if(nr >= 0 && nr < n && nc < m && nc >= 0 && grid[nr][nc] == 1){
+                    grid[nr][nc] = 2;
+                    fresh--;
+                    q.push({{nr, nc}, t+1});
+                }
             }
         }
         return fresh == 0 ? time : -1;
